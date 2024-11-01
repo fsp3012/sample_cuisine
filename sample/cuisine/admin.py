c4531be4ec8d7e9c7d1cafb0bcd3b8826d86cb26
fsp3012 <fakoredeabdulafeezolamide@gmail.com>
2024-11-01 17:41:41 +0000

    second commit

--- sample/cuisine/admin.py
+++ sample/cuisine/admin.py
@@ -3,6 +3,7 @@ from .models import Cuisine
 
 class CuisineAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'created', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('name', 'desc')
     prepopulated_fields = {'slug': ('name',)}
     raw_id_fields = ('author',)
